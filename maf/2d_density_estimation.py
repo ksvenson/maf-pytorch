fixed off by one error in mades.py

maf/2d_density_estimation.py
@@ -61,6 +61,8 @@ def main():
     data = np.load(dataset).astype(np.float32)
 
     print(data)
+    print(data.shape)
+    data /= 1e3
 
     train_data = torch.from_numpy(data[:900])
     test_data = torch.from_numpy(data[900:])
@@ -166,8 +168,8 @@ def main():
     # Save plot
 
     plt.savefig(png_name + ".png", dpi=500, bbox_inches="tight")
-    # plt.scatter(data[:, 0], data[:, 1], alpha=0.5, s=5)
-    # plt.savefig(png_name + '_with_pts' + ".png", dpi=500, bbox_inches="tight")
+    plt.scatter(data[:, 0], data[:, 1], alpha=0.5, s=5)
+    plt.savefig(png_name + '_with_pts' + ".png", dpi=500, bbox_inches="tight")
 
     # Rest now
 

maf/core/mades.py
@@ -51,7 +51,8 @@ def create_degrees(n_inputs, n_hiddens, input_order, mode):
 
     elif mode == 'sequential':
         for N in n_hiddens:
-            degrees_l = np.arange(N) % max(1, n_inputs - 1) + min(1, n_inputs - 1)
+            # degrees_l = np.arange(N) % max(1, n_inputs - 1) + min(1, n_inputs - 1)
+            degrees_l = np.arange(N) % n_inputs + 1
             degrees.append(degrees_l)
 
     else: