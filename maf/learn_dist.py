5bdaf76d5ad6444da0a019e09173e65cb832eb10
ksvenson <kai.svenson628@gmail.com>
2025-04-17 15:48:53 -0700

    Removed old code chunks

--- maf/learn_dist.py
+++ maf/learn_dist.py
@@ -86,82 +86,6 @@ def display_2d_uncond(dist, data, model, save_name, x, y, k, beta):
         # ax.scatter(sample[:, -2], sample[:, -1], alpha=0.25, s=3)
         # fig.savefig(f'{save_name}_with_sample_pts.png', **FIG_SAVE_OPTIONS)
 
-        # print(f'eng var: {torch.var(sample, dim=0)[-2]}')
-        # print(f'mag avg: {torch.mean(sample, dim=0)[-1]}')
-
-
-def plot_crit_surface_loop(dist, model, save_name, data_flat, k_list, beta_list):
-    assert model in ('made', 'made-mog', 'maf', 'maf-mog')
-    if model in ('maf', 'maf-mog'):
-        ms, vs = dist.get_ms_and_vs(data_flat)  # batch norm parameters
-    data = data_flat.reshape(21, 47, 1000, 4)
-
-    eng_var = np.full((k_list.size, beta_list.size), np.nan)
-    mag = np.full((k_list.size, beta_list.size), np.nan)
-
-    count = 0
-    for k_idx, k in enumerate(k_list):
-        for beta_idx, beta in enumerate(beta_list):
-
-            print('-'*50)
-            count += 1
-            print(f'{count}/{k_list.size * beta_list.size}')
-
-            sample = data[round(k_idx * (21 - 1) / (k_list.size - 1)), round(beta_idx * (47 - 1) / (beta_list.size - 1)), :, 2:].detach().numpy()
-            # sample = data[0, round(beta_idx * (47 - 1) / (beta_list.size - 1)), :, 2:].detach().numpy()
-            p75 = np.percentile(sample, 75, axis=0)
-            p25 = np.percentile(sample, 25, axis=0)
-            iqr = p75 - p25
-
-            upper = p75 + 1.5 * iqr
-            lower = p25 - 1.5 * iqr
-
-            o_max = np.max(sample, axis=0)
-            o_min = np.min(sample, axis=0)
-
-            print(upper)
-            print(lower)
-            print(o_max)
-            print(o_min)
-
-            eng_space = torch.linspace(lower[-2], upper[-2], int(1e3))
-            mag_space = torch.linspace(lower[-1], upper[-1], int(1e3))
-
-            eng_grid, mag_grid = torch.meshgrid(eng_space, mag_space, indexing='ij')
-            dist_input = torch.vstack([torch.full((eng_grid.numel(),), k), torch.full((eng_grid.numel(),), beta),  eng_grid.flatten(), mag_grid.flatten()]).T
-            with torch.no_grad():
-                if model in ('made', 'made-mog'):
-                    probs = dist.log_prob(dist_input).exp()
-                elif model in ('maf', 'maf-mog'):
-                    probs = dist.log_prob(dist_input, ms=ms, vs=vs).exp()
-            probs = probs.reshape(eng_space.numel(), mag_space.numel())
-
-            eng_avg = np.trapz(eng_space[:, np.newaxis] * probs, x=eng_space, axis=0)
-            eng_avg = np.trapz(eng_avg, x=mag_space, axis=0)
-
-            mag_avg = np.trapz(probs, x=eng_space, axis=0)
-            mag_avg = np.trapz(mag_space * mag_avg, x=mag_space, axis=0)
-
-            eng_sq_avg = np.trapz(eng_space[:, np.newaxis]**2 * probs, x=eng_space, axis=0)
-            eng_sq_avg = np.trapz(eng_sq_avg, x=mag_space, axis=0)
-
-            eng_var[k_idx, beta_idx] = eng_sq_avg - eng_avg**2
-            mag[k_idx, beta_idx] = mag_avg
-
-    print(k_list)
-
-    fig, ax = plt.subplots()
-    pcm = ax.pcolormesh(beta_list, k_list, eng_var, shading='nearest')
-    fig.colorbar(pcm)
-    ax.set(xlabel=r'$\beta$', ylabel=rf'$k_8$', title=f'Energy Variance')
-    fig.savefig(f'{save_name}_eng.png', **FIG_SAVE_OPTIONS)
-
-    fig, ax = plt.subplots()
-    pcm = ax.pcolormesh(beta_list, k_list, mag, shading='nearest')
-    fig.colorbar(pcm)
-    ax.set(xlabel=r'$\beta$', ylabel=rf'$k_8$', title=f'Magnetization')
-    fig.savefig(f'{save_name}_mag.png', **FIG_SAVE_OPTIONS)
-
 
 def plot_crit_surface(dist, model, save_name, data_flat, k_list, beta_list, method='integration'):
     assert model in ('made', 'made-mog', 'maf', 'maf-mog')
@@ -169,63 +93,26 @@ def plot_crit_surface(dist, model, save_name, data_flat, k_list, beta_list, meth
         # if model in ('maf', 'maf-mog'):
         #     ms, vs = dist.get_ms_and_vs(data_flat)  # batch norm parameters
 
-        if method == 'integration':
-            if model in ('maf', 'maf-mog'):
-                ms, vs = dist.get_ms_and_vs(data_flat)  # batch norm parameters
-            data = data_flat.reshape(k_list.size, beta_list.size, 1000, 4)
-            p75 = np.percentile(data, 75, axis=-2)
-            p25 = np.percentile(data, 25, axis=-2)
-            iqr = p75 - p25
+        mean = np.full((k_list.size, beta_list.size, 2), np.nan)
+        var = np.full((k_list.size, beta_list.size, 2), np.nan)
+        n = int(1e3)
+        for k_idx, k in enumerate(k_list):
+            for beta_idx, beta in enumerate(beta_list):
+                print((k_idx, beta_idx))
+                if model in ('maf', 'maf-mog'):
+                    data_points = data_flat[(data_flat[:, 0] == k) & (data_flat[:, 1] == beta)]
+                    ms, vs = dist.get_ms_and_vs(data_points)  # batch norm parameters
 
-            upper = p75 + 1.5 * iqr
-            lower = p25 - 1.5 * iqr
-
-            eng_space = torch.linspace(lower[-2], upper[-2], int(1e3))
-            mag_space = torch.linspace(lower[-1], upper[-1], int(1e3))
-
-            k_grid, beta_grid, eng_grid, mag_grid = torch.meshgrid(k_list, beta_list, eng_space, mag_space, indexing='ij')
-            dist_input = torch.vstack([k_grid, beta_grid, eng_grid.flatten(), mag_grid.flatten()]).T
-            with torch.no_grad():
+                # k_grid, beta_grid = torch.meshgrid(k_list, beta_list, indexing='ij')
+                conds = torch.vstack([torch.full((n,), k), torch.full((n,), beta)]).T
                 if model in ('made', 'made-mog'):
-                    probs = dist.log_prob(dist_input).exp()
+                    sample = dist.sample(n, conds=conds)
                 elif model in ('maf', 'maf-mog'):
-                    probs = dist.log_prob(dist_input, ms=ms, vs=vs).exp()
-            probs = probs.reshape(k_list.numel(), beta_list.numel(), eng_space.numel(), mag_space.numel())
-
-            eng_avg = np.trapz(eng_space[:, np.newaxis] * probs, x=eng_space, axis=-2)
-            eng_avg = np.trapz(eng_avg, x=mag_space, axis=-1)
-
-            mag_avg = np.trapz(probs, x=eng_space, axis=-2)
-            mag_avg = np.trapz(mag_space * mag_avg, x=mag_space, axis=-1)
-
-            eng_sq_avg = np.trapz(eng_space[:, np.newaxis]**2 * probs, x=eng_space, axis=-2)
-            eng_sq_avg = np.trapz(eng_sq_avg, x=mag_space, axis=-1)
-
-            eng_var = eng_sq_avg - eng_avg**2
-        elif method == 'sample':
-            mean = np.full((k_list.size, beta_list.size, 2), np.nan)
-            var = np.full((k_list.size, beta_list.size, 2), np.nan)
-            n = int(1e3)
-            for k_idx, k in enumerate(k_list):
-                for beta_idx, beta in enumerate(beta_list):
-                    print((k_idx, beta_idx))
-                    if model in ('maf', 'maf-mog'):
-                        data_points = data_flat[(data_flat[:, 0] == k) & (data_flat[:, 1] == beta)]
-                        ms, vs = dist.get_ms_and_vs(data_points)  # batch norm parameters
-
-                    # k_grid, beta_grid = torch.meshgrid(k_list, beta_list, indexing='ij')
-                    conds = torch.vstack([torch.full((n,), k), torch.full((n,), beta)]).T
-                    if model in ('made', 'made-mog'):
-                        sample = dist.sample(n, conds=conds)
-                    elif model in ('maf', 'maf-mog'):
-                        sample = dist.sample(n, ms=ms, vs=vs, conds=conds)
-                    mean[k_idx, beta_idx] = torch.mean(sample, dim=0)
-                    var[k_idx, beta_idx] = torch.var(sample, dim=0)
-            eng_var = var[..., -2]
-            mag_avg = mean[..., -1]
-
-    print(f'eng var: {eng_var[0, 0]}')
-    print(f'mag avg: {mag_avg[0, 0]}')
+                    sample = dist.sample(n, ms=ms, vs=vs, conds=conds)
+                mean[k_idx, beta_idx] = torch.mean(sample, dim=0)
+                var[k_idx, beta_idx] = torch.var(sample, dim=0)
+        eng_var = var[..., -2]
+        mag_avg = mean[..., -1]
 
     fig, ax = plt.subplots()
     pcm = ax.pcolormesh(beta_list, k_list, eng_var, shading='nearest')
