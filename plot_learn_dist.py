4415ef92b4cdacef34f6655911fe13dbb6662a11
ksvenson <kai.svenson628@gmail.com>
2026-02-14 18:25:55 -0600

    added function to find beta_c

--- plot_learn_dist.py
+++ plot_learn_dist.py
@@ -11,6 +11,39 @@ SC_IDX = (0, 1, 2)
 FCC_IDX = (3, 4, 5, 6, 7, 8)
 BCC_IDX = (9, 10, 11, 12)
 
+def peak_fit(x, x0, a, b, p):
+    return a / (1 + np.abs((x-x0)/b)**p)
+
+def find_beta_c(beta_list, var, window=20):
+    beta_c = np.full((var.shape[0], var.shape[2]), np.nan)
+    beta_c_err = np.full(beta_c.shape, np.nan)
+    for k_idx in range(var.shape[0]):
+        print(k_idx)
+        for obs_idx in range(var.shape[2]):
+            # sort = np.argsort(var[k_idx, :, obs_idx])
+            # beta_c_est = np.mean(beta_list[sort[-5:]])
+            # beta_c_idx_est = np.argmin(np.abs(beta_c_est - beta_list))
+            beta_c_idx_est = np.argmax(var[k_idx, :, obs_idx])
+            peak_idx_range = np.arange(max(0, beta_c_idx_est-window), min(var.shape[1], beta_c_idx_est+window+1))
+
+            p0=(beta_list[beta_c_idx_est], var[k_idx, beta_c_idx_est, obs_idx], peak_idx_range.size*(beta_list[1]-beta_list[0]), 2)
+            lower = [beta_list[peak_idx_range[0]], 0, 0, 0]
+            upper = [beta_list[peak_idx_range[-1]], 2*p0[1], beta_list[-1]-beta_list[0], np.inf]
+            try:
+                popt, pcov = sp.optimize.curve_fit(peak_fit, beta_list[peak_idx_range], var[k_idx, peak_idx_range, obs_idx], p0=p0, bounds=(lower, upper))
+            except:
+                print('fitting failed!')
+                fig, ax = plt.subplots()
+                ax.plot(beta_list, var[k_idx, :, obs_idx])
+                ax.plot(beta_list[peak_idx_range], peak_fit(beta_list[peak_idx_range], *p0))
+                fig.savefig('blahblah.svg', **FIG_SAVE_OPTIONS)
+                quit()
+
+            beta_c[k_idx, obs_idx] = popt[0]
+            beta_c_err[k_idx, obs_idx]  = np.sqrt(pcov[0, 0])
+    return beta_c, beta_c_err
+
+
 def display_2d_uncond(dist, data, model, save_name, x, y, k, beta):
     assert model in ('made', 'made-mog', 'maf', 'maf-mog')
 
@@ -51,7 +84,7 @@ def plot_crit_surface(dist, model, save_name, data_flat, k, beta, mh_var=None, m
     else:
         mean = np.full(beta.shape + (dist.base_dist.data_dim,), np.nan)
     var = np.full(mean.shape, np.nan)
-    n = 10**4
+    n = 10**5
     print(beta.shape)
     if False:
         with torch.no_grad():
@@ -78,58 +111,72 @@ def plot_crit_surface(dist, model, save_name, data_flat, k, beta, mh_var=None, m
 
     beta_list = beta.reshape(-1, beta.shape[-1])[0]
     k_list = k[FCC_IDX[-1]]
-    for dir_idx in range(len(FCC_IDX)):
-        fig, ax = plt.subplots()
-        pcm = ax.pcolormesh(beta_list, k_list, var[..., dir_idx], shading='nearest')
-        fig.colorbar(pcm)
-        ax.set(xlabel=r'$\beta$', ylabel=r'$K_6$', title=f'Direction {dir_idx+1} Energy Variance')
-        fig.savefig(f'{save_name}_eng_dir{dir_idx}.png', **FIG_SAVE_OPTIONS)
 
     fig, ax = plt.subplots()
-    pcm = ax.pcolormesh(beta_list, k_list, var[..., -1], shading='nearest')
+    ax.plot(beta_list, mh_var[0, :, -1], label=rf'$K_6 = {k_list[0]}$')
+    ax.plot(beta_list, mh_var[1, :, -1], label=rf'$K_6 = {k_list[1]}$')
+    ax.legend()
+    fig.savefig('blah.png', **FIG_SAVE_OPTIONS)
+
+    for dir_idx in range(len(FCC_IDX)):
+        fig, ax = plt.subplots()
+        pcm = ax.pcolormesh(beta_list, k_list, var[..., dir_idx], shading='nearest', rasterized=True)
+        fig.colorbar(pcm)
+        ax.set(xlabel=r'$\beta$', ylabel=r'$K_6$', title=f'Direction {dir_idx+1} Energy Variance')
+        fig.savefig(f'{save_name}_eng_dir{dir_idx}.svg', **FIG_SAVE_OPTIONS)
+
+    fig, ax = plt.subplots()
+    pcm = ax.pcolormesh(beta_list, k_list, var[..., -1], shading='nearest', rasterized=True)
     fig.colorbar(pcm)
     ax.set(xlabel=r'$\beta$', ylabel=r'$K_6$', title=f'Magnetization Variance')
-    fig.savefig(f'{save_name}_mag.png', **FIG_SAVE_OPTIONS)
+    fig.savefig(f'{save_name}_mag.svg', **FIG_SAVE_OPTIONS)
 
-    # for k_idx in np.arange(len(k_list)):
-    #     eng_peaks_idx, eng_peak_props = sp.signal.find_peaks(eng_var[k_idx])
-    #     mag_peaks_idx, mag_peak_props = sp.signal.find_peaks(mag_var[k_idx], height=(0, 0.002))
+    # beta_c = beta_list[np.argmax(var, axis=1)]
+    # mh_beta_c = beta_list[np.argmax(np.squeeze(mh_var), axis=1)]
+    # step = beta_list[1] - beta_list[0]
 
-    #     beta_c_eng.append(beta_list[eng_peaks_idx[0]])
-    #     beta_c_mag.append(beta_list[mag_peaks_idx[0]])
+    beta_c, beta_c_err = find_beta_c(beta_list, var)
+    mh_beta_c, mh_beta_c_err = find_beta_c(beta_list, mh_var)
 
-    beta_c = beta_list[np.argmax(var, axis=1)]
-    mh_beta_c = beta_list[np.argmax(np.squeeze(mh_var), axis=1)]
-    step = beta_list[1] - beta_list[0]
 
     fig, ax = plt.subplots()
     ax.plot(beta_c[:, -1], k_list, color='C0', linestyle='solid', label='MAF Mag.')
-    ax.fill_betweenx(k_list, beta_c[:, -1] - step, beta_c[:, -1] + step, alpha=0.2, color='C0')
+    ax.fill_betweenx(k_list, beta_c[:, -1] - beta_c_err[:, -1], beta_c[:, -1] + beta_c_err[:, -1], alpha=0.2, color='C0')
 
     ax.plot(mh_beta_c[:, -1], k_list, color='C0', linestyle='dotted', label='MH Mag.')
-    ax.fill_betweenx(k_list, mh_beta_c[:, -1] - step, mh_beta_c[:, -1] + step, alpha=0.2, color='C0')
+    ax.fill_betweenx(k_list, mh_beta_c[:, -1] - mh_beta_c_err[:, -1], mh_beta_c[:, -1] + mh_beta_c_err[:, -1], alpha=0.2, color='C0')
 
-    for idx, mh_idx in enumerate(FCC_IDX):
+    for idx in range(len(FCC_IDX)):
         ax.plot(beta_c[:, idx], k_list, color=f'C{idx+1}', linestyle='solid', label=f'MAF Dir. {idx+1} Eng. Var.')
-        ax.fill_betweenx(k_list, beta_c[:, idx] - step, beta_c[:, idx] + step, alpha=0.2, color=f'C{idx+1}')
+        ax.fill_betweenx(k_list, beta_c[:, idx] - beta_c_err[:, idx], beta_c[:, idx] + beta_c_err[:, idx], alpha=0.2, color=f'C{idx+1}')
 
-        ax.plot(mh_beta_c[:, mh_idx], k_list, color=f'C{idx+1}', linestyle='dotted', label=f'MH Dir. {idx+1} Eng. Var.')
-        ax.fill_betweenx(k_list, mh_beta_c[:, mh_idx] - step, mh_beta_c[:, mh_idx] + step, alpha=0.2, color=f'C{idx+1}')
+        ax.plot(mh_beta_c[:, idx], k_list, color=f'C{idx+1}', linestyle='dotted', label=f'MH Dir. {idx+1} Eng. Var.')
+        ax.fill_betweenx(k_list, mh_beta_c[:, idx] - mh_beta_c_err[:, idx], mh_beta_c[:, idx] + mh_beta_c_err[:, idx], alpha=0.2, color=f'C{idx+1}')
     ax.set(xlabel=r'$\beta$', ylabel=rf'$K_6$', title=r'$\beta_c$ Estimated with Maximum Variance')
     ax.legend()
-    fig.savefig(f'{save_name}_beta_c.png', **FIG_SAVE_OPTIONS)
+    fig.savefig(f'{save_name}_beta_c.svg', **FIG_SAVE_OPTIONS)
 
+    res_err = np.sqrt(beta_c_err**2 + mh_beta_c_err**2)
     fig, ax = plt.subplots()
     ax.plot(k_list, beta_c[:, -1] - mh_beta_c[:, -1], color='C0', label='Mag.')
-    ax.fill_between(k_list, beta_c[:, -1] - mh_beta_c[:, -1] - step*np.sqrt(2), beta_c[:, -1] - mh_beta_c[:, -1] + step*np.sqrt(2), alpha=0.2)
+    ax.fill_between(k_list, beta_c[:, -1] - mh_beta_c[:, -1] - res_err[:, idx], beta_c[:, -1] - mh_beta_c[:, -1] + res_err[:, idx], alpha=0.2)
 
-    for idx, mh_idx in enumerate(FCC_IDX):
-        ax.plot(k_list, beta_c[:, idx] - mh_beta_c[:, mh_idx], color=f'C{idx+1}', label=f'Dir. {idx+1} Eng. Var.')
-        ax.fill_between(k_list, beta_c[:, idx] - mh_beta_c[:, mh_idx] - step*np.sqrt(2), beta_c[:, idx] - mh_beta_c[:, mh_idx] + step*np.sqrt(2), alpha=0.2)
+    for idx in range(len(FCC_IDX)):
+        ax.plot(k_list, beta_c[:, idx] - mh_beta_c[:, idx], color=f'C{idx+1}', label=f'Dir. {idx+1} Eng. Var.')
+        ax.fill_between(k_list, beta_c[:, idx] - mh_beta_c[:, idx] - res_err[:, idx], beta_c[:, idx] - mh_beta_c[:, idx] + res_err[:, idx], alpha=0.2)
 
-    ax.set(xlabel=rf'$K_6$', ylabel=r'$\beta_c$ Residual')
+    ax.set(xlabel=r'$K_6$', ylabel=r'$\beta_c$ Residual')
     ax.legend()
-    fig.savefig(f'{save_name}_beta_c_res.png', **FIG_SAVE_OPTIONS)
+    fig.savefig(f'{save_name}_beta_c_res.svg', **FIG_SAVE_OPTIONS)
+
+    fig, ax = plt.subplots()
+    ax.plot(k_list, (beta_c[:, -1] - mh_beta_c[:, -1]) / res_err[:, -1], color='C0', label='Mag.')
+    for idx in range(len(FCC_IDX)):
+        ax.plot(k_list, (beta_c[:, idx] - mh_beta_c[:, idx]) / res_err[:, idx], color=f'C{idx+1}', label=f'Dir. {idx+1} Eng. Var.')
+    ax.fill_between(k_list, -3, 3, color='black', alpha=0.2)
+    ax.set(xlabel=r'$K_6$', ylabel=r'$\beta_c$ $\sigma$ Residual')
+    ax.legend()
+    fig.savefig(f'{save_name}_beta_c_sigma.svg', **FIG_SAVE_OPTIONS)
 
     plt.close()
 
@@ -177,5 +224,5 @@ if __name__ == '__main__':
 
     save_name = f'{tag}_e{epoch}_mh'
     
-    plot_crit_surface(dist, 'maf-mog', save_name, data, infer_k, infer_beta, mh_var=mh_res['var'], method='sample')
+    plot_crit_surface(dist, 'maf-mog', save_name, data, infer_k, infer_beta, mh_var=np.squeeze(mh_res['var'])[..., np.array(list(FCC_IDX) + [-1])], method='sample')
 
